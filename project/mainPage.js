ca162b05820205c8f10e201134dd9b75718f6ea8
yungbock <whs2769@gmail.com>
2024-02-21 21:59:54 +0900

    project01

---
+++ project/mainPage.js
@@ -0,0 +1,18 @@
+// 회원가입 로그인 마우스 올리면 회색으로 변경
+function changeButton1(x){
+    x.style.backgroundColor = "rgb(200, 200,200)";
+}
+function changeButton2(x){
+    x.style.backgroundColor = "white";
+}
+
+// 마우스를 올리면 다른 곳이 회색으로 변경
+const change = document.getElementsByClassName("header-Bar-Text");
+function change1(x){
+    x.style.backgroundColor = "rgb(200, 200,200)";
+}
+function change2(x){
+    x.style.backgroundColor = "white";
+}
+
+
